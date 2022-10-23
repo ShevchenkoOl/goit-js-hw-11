14ea88ea148c9ff5e984e7499943e98c37eda0fe
Oleksii Shevchenko <uzlabini@gmail.com>
2022-10-23 14:32:06 +0200

    23.10.2022

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,26 +1,22 @@
-// const BASE_URL = 'https://restcountries.com/v3.1/name';
 
-// export const fetchImages = name => {
-//     return fetch(
-//       `https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
-//     )
-//         .then(response => {
-//         if (!response.ok) {
-//           if (response.status === 404) {
-//             return [];
-//           }
-//           throw new Error(response.status);
-//         }
-//         return response.json();
-//       })
-//       .catch(error => {
-//         console.error(error);
-//       });
-//   };
-// const base_url = 'https://pixabay.com/api/';
-// const KEY = '30790248-497145c5d3b0c6576ca9c953f';
-// const container = document.querySelector('.list')
-// const cityName = document.querySelector('.city')
-// const form = document.querySelector('form')
+const KEY = '30790248-497145c5d3b0c6576ca9c953f';
+
+export const fetchImages = async (inputValue, pageNr) => {
+    return await fetch(
+      `https://pixabay.com/api/?key=${KEY}&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+    )
+      .then(async response => {
+        if (!response.ok) {
+          if (response.status === 404) {
+            return [];
+          }
+          throw new Error(response.status);
+        }
+        return await response.json();
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
 
