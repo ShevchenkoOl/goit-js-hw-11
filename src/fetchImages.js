528f6cd3c164afa449256b11119591e32b049832
Oleksii Shevchenko <uzlabini@gmail.com>
2022-10-25 22:10:44 +0200

    25.10.2022

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,22 +1,20 @@
+import axios from 'axios';
 
-const KEY = '30790248-497145c5d3b0c6576ca9c953f';
+// Fetch images from Pixabay API using Axios
 
-export const fetchImages = async (inputValue, pageNr) => {
-    return await fetch(
-      `https://pixabay.com/api/?key=${KEY}&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
-    )
-      .then(async response => {
-        if (!response.ok) {
-          if (response.status === 404) {
-            return [];
-          }
-          throw new Error(response.status);
-        }
-        return await response.json();
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
+async function fetchImages(name, page, perPage) {
+  const baseURL = 'https://pixabay.com/api/';
+  const key = '30790248-497145c5d3b0c6576ca9c953f';
 
+  try {
+    const response = await axios.get(
+      `${baseURL}?key=${key}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+    );
+    return response.data;
+  } catch (error) {
+    console.log('ERROR: ' + error);
+  }
+}
+
+export { fetchImages };
 
